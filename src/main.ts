85a33d532995236c10760eadc67efa2a0a3be462
João Lippi <joao.p.lippi@gmail.com>
2024-04-11 15:24:59 -0300

    #86drq6v4c - Giltadura AI - (ai-codereviewer) Experimentar API do Gemini para ver se aceita uma quantidade maior de tokens

--- src/main.ts
+++ src/main.ts
@@ -234,11 +234,20 @@ async function main() {
       prDetails.pull_number
     );
   } else if (eventData.action === "synchronize") {
-    diff = await getDiff(
-        prDetails.owner,
-        prDetails.repo,
-        prDetails.pull_number
-    );
+    const newBaseSha = eventData.before;
+    const newHeadSha = eventData.after;
+
+    const response = await octokit.repos.compareCommits({
+      headers: {
+        accept: "application/vnd.github.v3.diff",
+      },
+      owner: prDetails.owner,
+      repo: prDetails.repo,
+      base: newBaseSha,
+      head: newHeadSha,
+    });
+
+    diff = String(response.data);
   } else {
     console.log("Unsupported event:", process.env.GITHUB_EVENT_NAME);
     return;
